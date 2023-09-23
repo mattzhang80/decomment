ba076d57c6f3a60445dffc2e48a5495a70a548e9
matthewzhang <mz8189@princeton.edu>
2023-09-22 20:56:19 -0400

    final update

--- decomment.c
+++ decomment.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-// This program removes comments from a C program.
+/* State enum to track DFA states. Consists of an intial start state, 
+a begin comment state, an in comment state, an end comment state, an in string literal state, 
+an in character literal state, an escape string state, and an escape character state.
+*/
 enum State {
   START,
   BEGIN_COMMENT,
@@ -12,11 +15,12 @@ enum State {
   ESC_CHAR
 };
 
-// handleStart input when in the START state.
+/* handleStart input when in the START state. Takes the current character from stdin and 
+checks for the beginning of a comment, string literal, character literal, or newline character
+based on their respective initial characters. This is the starting state of the DFA*/
 enum State handleStart(int currChar) {
   enum State state;
   state = START;
-  // Check for beginning of comment, string literal, character literal, or newline.
   if (currChar == '/')
     state = BEGIN_COMMENT;
   else if (currChar == '\'') {
@@ -35,11 +39,12 @@ enum State handleStart(int currChar) {
   }
   return state;
 }
-// handleCommentStart handles input when in the BEGIN_COMMENT state.
+/* handleCommentStart handles input when in the BEGIN_COMMENT state, which is the state that 
+occurs after a '/' character is read. Check if input is in a comment, still beginning of comment, 
+in a character literal, string literal, or none of above, based on respective characters*/
 enum State handleCommentStart(int currChar) {
   enum State state;
   state = BEGIN_COMMENT;
-  // Check for in a comment, beginning of comment, character literal, string literal, or n/a.
   if (currChar == '*') {
     putchar(' ');
     state = IN_COMMENT;
@@ -65,21 +70,23 @@ enum State handleCommentStart(int currChar) {
   }
   return state;
 }
-// handleInComment handles input when in the IN_COMMENT state.
+/* handleInComment handles input when in the IN_COMMENT state, which is the state that occurs after a '/*' is read.
+In this function, it checks if it is reaching the end of comment, a newline, or still in a comment. */
+
 enum State handleInComment(int currChar) {
   enum State state;
   state = IN_COMMENT;
-  // Check for end of comment or newline.
   if(currChar == '\n')
     putchar('\n');
   else if (currChar == '*')
     state = END_COMMENT;
   return state; 
 }
-// handleEndComment handles input when in the END_COMMENT state.
+/* handleEndComment handles input when in the END_COMMENT state, which is the state that occurs after 
+input is in a comment and the current character is a *. In this function, it verifies for end of comment, 
+checks for newline, or if it is still in a comment.*/
 enum State handleEndComment(int currChar) {
   enum State state;
-  // Check for end of comment, newline, or still in comment.
   if (currChar == '/'){
     state = START;
     return state;
@@ -94,11 +101,11 @@ enum State handleEndComment(int currChar) {
     state = IN_COMMENT;
   return state;
 }
-// handleInStringLit handles input when in the IN_STRING_LIT state.
+/* handleInStringLit handles input when in the IN_STRING_LIT state. This occurs when input reads a ".
+This function checks for end of string literal or escape character, or if it is still in a string literal. */
 enum State handleInStringLit(int currChar) {
   enum State state;
   state = IN_STRING_LIT;
-  // Check for end of string literal or escape character.
   if (currChar == '"')
     state = START;
   else if (currChar == '\\')
@@ -106,11 +113,11 @@ enum State handleInStringLit(int currChar) {
   putchar(currChar);
   return state;
 }
-// handleInCharLit handles input when in the IN_CHAR_LIT state.
+/* handleInCharLit handles input when in the IN_CHAR_LIT state, which occurs after a '. 
+It checks for the end of character literal or escape character, or if it is still in a literal. */
 enum State handleInCharLit(int currChar) {
   enum State state;
   state = IN_CHAR_LIT;
-  // Check for end of character literal or escape character.
   if (currChar == '\'')
     state = START;
   else if (currChar == '\\')
@@ -118,17 +125,25 @@ enum State handleInCharLit(int currChar) {
   putchar(currChar);
   return state;
 }
-// handleEscString handles input when in the ESC_STRING state.
+/* handleEscString handles input when in the ESC_STRING state, when input reads a \ in the string literal.
+It simply puts the current character to the output and goes back to the string literal.*/
+
 enum State handleEscString(int currChar) {
   putchar(currChar);
   return IN_STRING_LIT;
 }
-// handleEscChar handles input when in the ESC_CHAR state.
+/* handleEscChar handles input when in the ESC_CHAR state, when input reads a \ in the character literal. 
+It simply puts the current character to the output and goes back to the character literal.*/
 enum State handleEscChar(int currChar) {
   putchar(currChar);
   return IN_CHAR_LIT;
 }
-// main handles int, beginning in the START state.
+/* Read a character from stdin until everything is read. While tracking the current line number, 
+begin in the START state and switch to the appropriate state based on the current character.
+Removes comments from stdin and prints the remaining result to stdout.
+Return EXIT_SUCCESS if the program runs successfully, and EXIT_FAILURE if it does not.
+Does not run successfully if there are unterminated comments.
+*/
 int main() {
   // Initialize variables.
   enum State state;
@@ -136,13 +151,13 @@ int main() {
   int currline = 1;
   int linestart = 0;
   state = START;
-  // Loop through DFA using input.
+  /* Loop through DFA using input. */
   while ((currChar = getchar()) != EOF) {
-    // Track line number
+    // Tracks line number
     if(currChar == '\n'){
       currline++;
     }
-    // Switch statement to handle the DFA.
+    /* Switch statement to handle the DFA states. */
     switch (state) {
       case START:
         state = handleStart(currChar);
@@ -171,12 +186,12 @@ int main() {
         break;
     }
   }
-  // Check for unterminated comment.
+  /* Check for unterminated comment. */
   if(state == IN_COMMENT || state == END_COMMENT) {
     fprintf(stderr, "Error: line %d: unterminated comment\n", linestart);
     return EXIT_FAILURE;
   }
-  // Check for unterminated string literal.
+  /* Check for unterminated string literal. */
   if(state == BEGIN_COMMENT){
     putchar('/');
   }
